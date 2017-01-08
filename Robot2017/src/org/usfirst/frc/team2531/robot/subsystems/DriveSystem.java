052a4ff9df359fa1958ba9002e53dcfca12c8c78
chase <chase@chases-hp-pavilion.hitronhub.home>
2017-01-07 19:19:02 -0600

    started project and added drive

---
+++ Robot2017/src/org/usfirst/frc/team2531/robot/subsystems/DriveSystem.java
@@ -0,0 +1,32 @@
+package org.usfirst.frc.team2531.robot.subsystems;
+
+import org.usfirst.frc.team2531.robot.commands.ThreeAxisDrive;
+
+import edu.wpi.first.wpilibj.Jaguar;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class DriveSystem extends Subsystem {
+
+	// Put methods for controlling this subsystem
+	// here. Call these from Commands.
+	private Jaguar FL = new Jaguar(0);
+	private Jaguar FR = new Jaguar(1);
+	private Jaguar BL = new Jaguar(2);
+	private Jaguar BR = new Jaguar(3);
+
+	public void initDefaultCommand() {
+		// Set the default command for a subsystem here.
+		// setDefaultCommand(new MySpecialCommand());
+		setDefaultCommand(new ThreeAxisDrive());
+	}
+
+	public void axisdrive(double x, double y, double r) {
+		FL.set(-y + x + r);
+		FR.set(y + x + r);
+		BL.set(-y - x + r);
+		BR.set(y - y + r);
+	}
+}
