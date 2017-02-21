9ab708688814d8da4ce302b460e134899a10758e
chase <chase@ChaseLaptop>
2017-02-21 09:14:14 -0600

    update

--- Robot2017/src/org/usfirst/frc/team2531/robot/commands/Drive.java
+++ Robot2017/src/org/usfirst/frc/team2531/robot/commands/Drive.java
@@ -2,6 +2,7 @@ package org.usfirst.frc.team2531.robot.commands;
 
 import org.usfirst.frc.team2531.robot.OI;
 import org.usfirst.frc.team2531.robot.Robot;
+import org.usfirst.frc.team2531.robot.RobotMap;
 
 import edu.wpi.first.wpilibj.command.Command;
 
@@ -16,21 +17,20 @@ public class Drive extends Command {
 	}
 
 	protected void execute() {
-		// double a = Math.toRadians(RobotMap.imu.getAngleZ() / 4);
+		double a = Math.toRadians(RobotMap.imu.getAngleZ() / 4);
 		if (OI.gamepad.getRawAxis(3) > 0) {
-			Robot.drive.axisdrive(OI.gamepad.getRawAxis(4) / (4 * OI.gamepad.getRawAxis(3)),
-					OI.gamepad.getRawAxis(1) / (4 * OI.gamepad.getRawAxis(3)),
-					OI.gamepad.getRawAxis(0) / (4 * OI.gamepad.getRawAxis(3)));
-			// Robot.drive.axisdrive((Math.cos(a) * OI.gamepad.getRawAxis(4)) /
-			// (4 * OI.gamepad.getRawAxis(3)),
-			// (Math.sin(a) * OI.gamepad.getRawAxis(1)) / (4 *
+			// Robot.drive.axisdrive(OI.gamepad.getRawAxis(4) / (4 *
 			// OI.gamepad.getRawAxis(3)),
+			// OI.gamepad.getRawAxis(1) / (4 * OI.gamepad.getRawAxis(3)),
 			// OI.gamepad.getRawAxis(0) / (4 * OI.gamepad.getRawAxis(3)));
+			Robot.drive.axisdrive((Math.cos(a) * OI.gamepad.getRawAxis(4)) / (4 * OI.gamepad.getRawAxis(3)),
+					(Math.sin(a) * OI.gamepad.getRawAxis(1)) / (4 * OI.gamepad.getRawAxis(3)),
+					OI.gamepad.getRawAxis(0) / (4 * OI.gamepad.getRawAxis(3)));
 		} else {
-			Robot.drive.axisdrive(OI.gamepad.getRawAxis(4), OI.gamepad.getRawAxis(1), OI.gamepad.getRawAxis(0));
-			// Robot.drive.axisdrive(Math.cos(a) * OI.gamepad.getRawAxis(4),
-			// Math.sin(a) * OI.gamepad.getRawAxis(1),
-			// OI.gamepad.getRawAxis(0));
+			// Robot.drive.axisdrive(OI.gamepad.getRawAxis(4),
+			// OI.gamepad.getRawAxis(1), OI.gamepad.getRawAxis(0));
+			Robot.drive.axisdrive(Math.cos(a) * OI.gamepad.getRawAxis(4), Math.sin(a) * OI.gamepad.getRawAxis(1),
+					OI.gamepad.getRawAxis(0));
 		}
 	}
 
