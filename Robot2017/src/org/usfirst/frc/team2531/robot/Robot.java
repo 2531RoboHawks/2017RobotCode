6b2766dbd9a35977bacb245d6195d87f86f18ca1
chase <chase@ChaseLaptop>
2017-12-18 08:06:45 -0600

    update

--- Robot2017/src/org/usfirst/frc/team2531/robot/Robot.java
+++ Robot2017/src/org/usfirst/frc/team2531/robot/Robot.java
@@ -4,6 +4,7 @@ package org.usfirst.frc.team2531.robot;
 import org.usfirst.frc.team2531.robot.commands.DriveSquare;
 import org.usfirst.frc.team2531.robot.commands.TimeDrive;
 import org.usfirst.frc.team2531.robot.commands.TimeDriveGyro;
+import org.usfirst.frc.team2531.robot.commands.TrackR;
 import org.usfirst.frc.team2531.robot.commands.Turn2Angle;
 import org.usfirst.frc.team2531.robot.subsystems.DriveSystem;
 
@@ -13,11 +14,15 @@ import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frclib.vision.Vision;
 
 public class Robot extends IterativeRobot {
 
 	public static OI oi;
 	public static DriveSystem drive;
+
+	public static int min1 = 100, min2 = 0, min3 = 0, max1 = 255, max2 = 20, max3 = 20, w = 720, h = 480,
+			minsize = 1000;
 
 	SendableChooser<Command> auto;
 	Command autocommand;
@@ -30,6 +35,7 @@ public class Robot extends IterativeRobot {
 		RobotMap.imu.calibrate();
 		RobotMap.imu.reset();
 		initSmartDashboard();
+		RobotMap.cam0 = new Vision("cam0", 0, w, h);
 	}
 
 	@Override
@@ -85,7 +91,8 @@ public class Robot extends IterativeRobot {
 		auto.addObject("Time Drive 1sec", new TimeDrive(1000, 0.5));
 		auto.addObject("Time Drive Gyro 2sec", new TimeDriveGyro(2000, 0.5));
 		auto.addObject("Turn 90deg", new Turn2Angle(90));
-		auto.addObject("Drive Square", new DriveSquare(2000,0.5));
+		auto.addObject("Drive Square", new DriveSquare(2000, 0.5));
+		auto.addObject("VT", new TrackR(false));
 		SmartDashboard.putData("Autonomous Mode", auto);
 		SmartDashboard.putNumber("AngleX", RobotMap.imu.getAngleX());
 		SmartDashboard.putNumber("AngleY", RobotMap.imu.getAngleY());
--- Robot2017/src/org/usfirst/frc/team2531/robot/RobotMap.java
+++ Robot2017/src/org/usfirst/frc/team2531/robot/RobotMap.java
@@ -1,9 +1,11 @@
 package org.usfirst.frc.team2531.robot;
 
 import frclib.sensors.ADIS16448_IMU;
+import frclib.vision.Vision;
 
 public class RobotMap {
 
 	public static ADIS16448_IMU imu = new ADIS16448_IMU();
+	public static Vision cam0;
 
 }
